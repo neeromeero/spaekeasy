5734bcda1bf68655a2a3b2fa215d4119f1882280
NeeroMeero <neeromeero@yandex.ru>
2024-11-13 15:54:03 +0500

    Bottombar without chat and camera

--- app/src/main/java/ru/neeromeero/speakeasy/MainActivity.kt
+++ app/src/main/java/ru/neeromeero/speakeasy/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.primarySurface
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import ru.neeromeero.speakeasy.ui.theme.SpeakEasyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpeakEasyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    bottomBar = {
+                        BottomNavigationBar()
+                    }
+                ) { innerPadding ->
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun BottomNavigationBar(){
+        BottomNavigation(
+            backgroundColor = MaterialTheme.colors.primarySurface,
+            contentColor = MaterialTheme.colors.onPrimary,
+        ) {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_translate),
+                        contentDescription = "Translate"
+                    )
+                },
+                selected = false,
+                onClick = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpeakEasyTheme {
-        Greeting("Android")
+                }
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_history),
+                        contentDescription = "History"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        ImageVector.vectorResource(id = R.drawable.ic_fav),
+                        contentDescription = "Favorite"
+                    )
+                },
+                selected = false,
+                onClick = {
+
+                }
+            )
+        }
     }
 }
